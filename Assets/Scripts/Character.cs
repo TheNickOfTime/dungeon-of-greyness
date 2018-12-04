42ec23a8dc4235a1150eba52a044244facec6228
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2018-12-04 14:10:24 -0800

    Week 1

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	//Components-------------------------------------------------------------------------------------------------------/
+	private Animator m_Anim;
+	public Animator Anim
+	{
+		get
+		{
+			return m_Anim;
+		}
+	}
+
+	private Rigidbody2D m_Rig;
+	
+	//Values-----------------------------------------------------------------------------------------------------------/
+	[SerializeField] private float m_MoveSpeed;
+	public bool m_CanMove = true;
+
+	private void Awake()
+	{
+		m_Anim = GetComponent<Animator>();
+		m_Rig = GetComponent<Rigidbody2D>();
+	}
+
+	public void Move(Vector2 direction)
+	{
+		if (m_CanMove)
+		{
+			m_Rig.velocity = direction * m_MoveSpeed;
+		}
+		else
+		{
+			m_Rig.velocity = Vector2.zero;
+		}
+	}
+
+	public void Attack()
+	{
+		Anim.SetTrigger("Attack");
+		
+	}
+}
